2f8f40793368a87837ac95a569f76a670ecf034e
Artemiss1509 <stanza-grout54@icloud.com>
2026-04-30 12:10:00 +0530

    DSA Practice 30th April 2026

---
+++ TwoPointers/MergeSortedArray.js
@@ -0,0 +1,15 @@
+var merge = function(nums1, m, nums2, n) {
+    let left = m-1
+    let right = n-1
+    let point = m+n-1
+    while(right>=0){
+        if(left>=0 && nums1[left] > nums2[right]){
+            nums1[point] = nums1[left]
+            left--
+        }else {
+            nums1[point] = nums2[right]
+            right--
+        }
+        point--
+    }
+};
